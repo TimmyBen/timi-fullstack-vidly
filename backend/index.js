fcd555fee4f31e9426290854af461fd026e2a692
Timi Adewumi <nucleuscaesar@yahoo.com>
2022-06-26 05:21:08 +0100

    Made Final Adjustments

--- backend/index.js
+++ backend/index.js
@@ -5,5 +5,5 @@ db.connect().then(() => {
   console.log("Connected to MongoDB: " + db.url);
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server started on port ${port}...`));
